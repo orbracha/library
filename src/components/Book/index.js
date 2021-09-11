4c6b2f49c7cf0ca79530de0b8b225cb5e9dd6e91
or bracha <orbracha2308@gmail.com>
2021-09-11 22:14:13 +0300

    fixed CSS media query

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -4,7 +4,9 @@ export default function Book({ bookDetails, onSelected }) {
   const { title, img } = bookDetails;
   return (
     <div onClick={() => onSelected(bookDetails)} className="book-container">
-      <h1 className="book-title">{title}</h1>
+      <h1 title={title} className="book-title">
+        {title}
+      </h1>
       <img alt="book-img" className="book-img" src={img || emptyImgBook} />
     </div>
   );
